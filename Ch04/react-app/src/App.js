fe9f27b5e41518f364864128fe11a22856de8f34
Jeanaica Suplido <sjeanaica@gmail.com>
2023-02-14 11:22:13 +0800

    ✨ (0305): Working with useEffect

--- Ch04/react-app/src/App.js
+++ Ch04/react-app/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // via array destructuring
   const [emotion, setEmotion] = useState('happy');
+
+  useEffect(
+    () => {
+      console.log(`It's ${emotion} right now`);
+    },
+    // dependency array
+    []
+  );
+
   return (
     <div className='App'>
       <h1>Current emotion is {emotion}</h1>
