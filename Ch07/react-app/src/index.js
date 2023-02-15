58b9d86caf32a6cea8288896abaaf414a6ad1d01
Jeanaica Suplido <sjeanaica@gmail.com>
2023-02-16 00:05:25 +0800

    🎨 (0704): Nesting links with React Router v6

--- Ch07/react-app/src/index.js
+++ Ch07/react-app/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import { App, About, Contact } from './App';
+import { App, About, Contact, History } from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='/about' element={<About />} />
+      <Route path='/about' element={<About />}>
+        <Route path='history' element={<History />} />
+      </Route>
       <Route path='/contact' element={<Contact />} />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
